7721742e0c05893b2261fabbd270c3b81dc81ddf
soyMat <mazuelamateo16@gmail.com>
2026-04-07 22:17:12 -0300

    Resolucion modulo de Listas

--- src/listas.py
+++ src/listas.py
@@ -7,24 +7,21 @@ def suma_lista(numeros: list) -> int | float:
     """
     Retorna la suma de todos los elementos de la lista.
     """
-    # TU CÓDIGO AQUÍ
-    pass
+    return sum(numeros)
 
 
 def filtrar_pares(numeros: list) -> list:
     """
     Retorna una nueva lista con solo los números pares.
     """
-    # TU CÓDIGO AQUÍ
-    pass
+    return [n for n in numeros if n % 2 == 0]
 
 
 def invertir_lista(lista: list) -> list:
     """
     Retorna la lista invertida SIN modificar la original.
     """
-    # TU CÓDIGO AQUÍ
-    pass
+    return lista[::-1]
 
 
 def eliminar_duplicados(lista: list) -> list:
@@ -32,8 +29,7 @@ def eliminar_duplicados(lista: list) -> list:
     Retorna una nueva lista sin elementos duplicados,
     manteniendo el orden de primera aparición.
     """
-    # TU CÓDIGO AQUÍ
-    pass
+    return list(dict.fromkeys(lista))
 
 
 def aplanar_lista(lista: list) -> list:
@@ -41,5 +37,4 @@ def aplanar_lista(lista: list) -> list:
     Dada una lista de listas, retorna todos los elementos en una sola lista.
     Ejemplo: aplanar_lista([[1,2],[3,4]]) -> [1, 2, 3, 4]
     """
-    # TU CÓDIGO AQUÍ
-    pass
+    return [item for sublist in lista for item in sublist]
